e5580f1e738a550c72be828cbb0e1c843365c03b
Unknown <ttomass@gmail.com>
2018-08-07 17:59:00 +0300

    Created a oroject

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
